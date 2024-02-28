f888bbced5906bf49534dd57245c2a801930387a
Suraj <hingadesuraj@gmail.com>
2024-02-28 19:00:49 +0530

    impliment onchange

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 import Todo from "./components/Todo";
 
 function App() {
+ const [title,setTitle] = useState("");
+ const [description,setDescription] = useState("");
+ const [mark,setMark] = useState(false);
+
+  const handleSubmit = () =>{
+    alert(title,description,mark)
+  }
+
+ 
+
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">
@@ -14,17 +26,24 @@ function App() {
           <input
             className="border-2 border-cyan-800 outline-none rounded-md"
             type="text"
+            name="title"
+            value={title}
+            onChange={(e)=>setTitle(e.target.value)}
+
           />
           Description{" "}
           <input
             className="border-2 border-cyan-800 outline-none rounded-md"
             type="text"
+            name="description"
+            value={description}
+            onChange={(e)=>setDescription(e.target.value)}
           />
           <div>
             Mark Done{" "}
-            <input className="p-2 m-2 gap-3" type="checkbox" name="" id="" />
+            <input className="p-2 m-2 gap-3" type="checkbox" name="mark" id="" value={mark} onChange={(e)=>setMark(e.target.value)}  />
           </div>
-          <button className="border-2 border-black bg-white text-black hover:bg-black hover:text-white p-2 rounded-md">
+          <button onClick={handleSubmit} className="border-2 border-black bg-white text-black hover:bg-black hover:text-white p-2 rounded-md">
             Submit
           </button>
         </div>
