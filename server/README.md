1c8b74567ee0cfb14cce42e1b3fd5e81695c5451
Suraj <hingadesuraj@gmail.com>
2024-02-28 13:19:18 +0530

    impliment boiler plate code for todo server

---
+++ server/README.md
@@ -0,0 +1,8 @@
+## Todo App
+
+This project contains a simple TODO application 
+It has the following features - 
+
+-Anyone can create a todo
+-Any one can see their existing todos
+-Any one can mark a todo as done
--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const connnectToDb = require('./db');
+const express = require("express");
+const cors = require("cors");
+const connnectToDb = require("./db");
+const zod = require("zod");
 const app = express();
 const PORT = 3000;
-
 
 app.use(cors());
 app.use(express.json());
 connnectToDb();
 
+app.get("/", (req, res) => {
+  res.send("Server is working.....!");
+});
+ 
+app.post("/todo", (req, res) => {});
 
-app.get("/",(req,res)=>{
-    res.send("Server is working.....!")
-})
+app.get("/todos", (req, res) => {});
 
+app.put("/completed", (req, res) => {});
 
-
-app.listen(PORT,()=>{
-    console.log("Server is running on "+"http://localhost:3000")
-})
+app.listen(PORT, () => {
+  console.log("Server is running on " + "http://localhost:3000");
+});
